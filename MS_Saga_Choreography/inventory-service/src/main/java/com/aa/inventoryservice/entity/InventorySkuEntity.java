09eff168ea07076db6df7602f337b25c8d6cf851
Asim Ahmed <sendtoasimOff@gmail.com>
2023-09-20 20:56:50 +0500

    commnit 2

--- MS_Saga_Choreography/inventory-service/src/main/java/com/aa/inventoryservice/entity/InventorySkuEntity.java
+++ MS_Saga_Choreography/inventory-service/src/main/java/com/aa/inventoryservice/entity/InventorySkuEntity.java
@@ -1,4 +1,25 @@
 package com.aa.inventoryservice.entity;
 
-public class InventorySkuEntity {
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=false)
+@Entity
+@Table(name = "inv_skus")
+public class InventorySkuEntity extends InventoryBaseEntity {
+	private Long skuId;
+	private Long itemId;
+    private String sku;
+    //private String barcode;
+    private Integer statusId;
+    private BigDecimal reorderQuantityThreshold; 
+    private BigDecimal reorderQuantity; 
+    private BigDecimal maximumReorderQuantity;
+    private Long siteId;
+    private Long categoryId;
 }
